0867ee53d5da3c29bd993b1fe3d7b3b50577875f
Thomas Kraxner <71735135+Kraego@users.noreply.github.com>
2025-06-12 09:37:27 +0000

    unit test no working

--- tests/cpu/cpu-test.cpp
+++ tests/cpu/cpu-test.cpp
@@ -1,10 +1,14 @@
 #include <gtest/gtest.h>
-#include "../src/cpu/cpu.h"
+#include "cpu.h"
 
 TEST(cpu_reset_test, cpu_reset) {
     cpu_t cpu_state = {};
 
     cpu_reset(&cpu_state);
-    // Add assertions here
-    EXPECT_EQ(1, 1);
+    EXPECT_EQ(cpu_state.r.af, 0x01B0);
+    EXPECT_EQ(cpu_state.r.bc, 0x0013);
+    EXPECT_EQ(cpu_state.r.de, 0x00D8);
+    EXPECT_EQ(cpu_state.r.hl, 0x014D);
+    EXPECT_EQ(cpu_state.sp,   0xFFFE);
+    EXPECT_EQ(cpu_state.pc,   0x0100);
 }
